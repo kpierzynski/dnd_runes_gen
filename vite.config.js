2e5e43c332028a820439014a0ce81e6d5d499d86
kpierzynski <kpierzynski@hotmail.com>
2022-11-06 18:30:57 +0100

    Added visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { visualizer } from "rollup-plugin-visualizer";
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/dnd_runes_gen/",
-	plugins: [react()]
+	plugins: [react(), visualizer()]
 });
